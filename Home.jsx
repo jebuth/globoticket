eec22be725b002ddf4f56d96bfbf21033b2286ed
Justin Buth <jb@Justins-MacBook-Pro.local>
2021-07-12 21:05:15 -0500

    added stylesheet

--- Home.jsx
+++ Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {View, Image, Text} from 'react-native';
+import {View, Image, Text, StyleSheet} from 'react-native';
 
 const Home = (props) => {
     return(
-        <View style={{flexDirection:'column'}}>
+        <View style={styles.container}>
             <Image 
                 style = {styles.globologo}
                 source={require('./images/_Export_globoticket-bug-black.png')}
@@ -18,5 +18,33 @@ const Home = (props) => {
 };
 
 const introText = 'some text';
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: 20,
+    },
+    textContainer: {
+        textAlign: 'center',
+        paddingTop: 10
+    },
+    globologo: {
+        height: 100,
+        width: 150
+    },
+    title: {
+        fontFamily: 'Ubuntu-Regular'
+    },
+    subtitle: {
+        paddingTop: 5,
+        fontFamily: 'Ubuntu-Regular'
+    },
+    content: {
+        fontFamily: 'Ubuntu-Light',
+        fontWeight: 300,
+    }
+
+});
 
 export default Home;
